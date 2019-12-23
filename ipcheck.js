8200488204acc46513debf4e19ce4b3245428cc6
“alexandro591” <alexandrotapiaflores@gmail.com>
2019-12-23 02:39:37 -0500

    functions

--- ipcheck.js
+++ ipcheck.js
@@ -1,14 +1,14 @@
 function ipCheck(){
     let ipv6info;
     let ipv4info;
-    let userInformation;
     $.getJSON('https://ip6.seeip.org/geoip', function(data){
         ipv6info=data;
     }).then(data=>{
         $.getJSON('https://ip4.seeip.org/geoip', function(data){
             ipv4info=data;
         }).then(data=>{
-                console.log(finished);
+                console.log(ipv4);
+                console.log(ipv6);
                 userInformation=navigator.userAgent;
                 return [ipv4info,ipv6info];
             });
