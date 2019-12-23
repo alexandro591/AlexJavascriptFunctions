58c3338ddf14090022f9d55368e62c5d03a4c125
“alexandro591” <alexandrotapiaflores@gmail.com>
2019-12-23 02:15:20 -0500

    functions

--- sendemail.js
+++ sendemail.js
@@ -1,8 +1,7 @@
 var Email = { send: function (a) { return new Promise(function (n, e) { a.nocache = Math.floor(1e6 * Math.random() + 1), a.Action = "Send"; var t = JSON.stringify(a); Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (e) { n(e) }) }) }, ajaxPost: function (e, n, t) { var a = Email.createCORSRequest("POST", e); a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"), a.onload = function () { var e = a.responseText; null != t && t(e) }, a.send(n) }, ajax: function (e, n) { var t = Email.createCORSRequest("GET", e); t.onload = function () { var e = t.responseText; null != n && n(e) }, t.send() }, createCORSRequest: function (e, n) { var t = new XMLHttpRequest; return "withCredentials" in t ? t.open(e, n, !0) : "undefined" != typeof XDomainRequest ? (t = new XDomainRequest).open(e, n) : t = null, t } };
 
-class EmailTools {
-	
-	sendSimpleEmail(to,from,subject,body,token,confirmation){
+EmailTools = {
+	sendSimpleEmail : function(to,from,subject,body,token,confirmation){
 		body=body.replace(/\n/g,"<br>");
 		Email.send({
 			SecureToken : token,
@@ -26,10 +25,6 @@ class EmailTools {
 		);
 	}
 }
-
-Email.sendSimpleEmail
-
-
 
 //this js function is a simplified version of https://www.smtpjs.com/
 //go there to generate your token with your  credentials
